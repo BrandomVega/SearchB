1644a63157ee3131cf4b224c60b2434866cf1415
brandom <bvegae1800@alumno.ipn.mx>
2023-12-05 17:09:10 -0600

    EncuentraTextoImagen

--- imageLecture.py
+++ imageLecture.py
@@ -3,11 +3,11 @@ from PIL import Image
 import re
 import time
 import os
+
 def encontrar_texto_entre_comillas(cadena):
     patron = r'"(.*?)"'
     coincidencias = re.findall(patron, cadena)
     return coincidencias
-
 
 def imageToText(textoNormalizado, path):
     inicio = time.time()
@@ -15,6 +15,9 @@ def imageToText(textoNormalizado, path):
     imagen = Image.open(path)
     texto = pytesseract.image_to_string(imagen).lower()
     textoWords = texto.split()
+
+    print(textoWords)
+
     cuenta = 0
     for palabra in textoNormalizado:
         if palabra in textoWords: 
@@ -23,33 +26,28 @@ def imageToText(textoNormalizado, path):
     end = time.time()
     print(f"    Tiempo {round(end-inicio, 4)} segundos ")
 
-    print(f"    Coincidencia: {cuenta/len(textoNormalizado)*100}%")
+    print(f"    Coincidencia: {cuenta}")
 
+
+
+#PROCESO: ENCUENTRA TEXTO EN IMAGENES
+from gui import selectDirectory as getDir
+
+directoryPath = getDir.getDirectory()
+print(directoryPath)
+
+textQuery= input("Buscar: \n>> ")
+searchText = encontrar_texto_entre_comillas(textQuery)[0].lower().split()
 
 extensiones_imagen = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.webp']
 
-texto = input("Buscar: \n>> ")
-textoNormalizado = encontrar_texto_entre_comillas(texto)[0].lower().split()
-
-direccionCarpeta = input("Ingresa el path de la carpeta a buscar: ").strip()
-print("")
-#print(direccionCarpeta)
-
-#carpeta = r"C:\Users\Brandom\Pictures\Screenshots"
-nombres_archivos = os.listdir(direccionCarpeta)
+nombres_archivos = os.listdir(directoryPath)
 for nombre_archivo in nombres_archivos:
     ext = os.path.splitext(nombre_archivo)[-1].lower()
     if ext in extensiones_imagen:
-        imageToText(textoNormalizado, direccionCarpeta + "\\" +nombre_archivo)
+        imageToText(searchText, directoryPath + "\\" +nombre_archivo)
     else:
         #print("--------------   ESTE NO:",nombre_archivo)
         continue
 
-    #"C:\Users\Brandom\Pictures\forProyect\epico.jpg"
 
-
-
-
-
-
-#Practica 2 Algoritmo de enjambre de particulas
