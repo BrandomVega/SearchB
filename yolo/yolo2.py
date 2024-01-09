c560d4f30f92cf8293a44a7f00b28ef4677c55a7
brandom <bvegae1800@alumno.ipn.mx>
2024-01-09 17:02:12 -0600

    yoloDir Avance

--- yolo/yolo2.py
+++ yolo/yolo2.py
@@ -3,13 +3,12 @@ from ultralytics import YOLO
 import json 
 import os
 
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 #Import proyectLibraries
 import sys
 sys.path.append(r'C:\Users\Brandom\Documents\1Proyecto')
 from gui import selectDirectory as getDir
-
-
-
 
 #Get directory to search
 directoryPath = getDir.getDirectory()
@@ -17,33 +16,55 @@ extensiones_imagen = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', 
 nombres_archivos = os.listdir(directoryPath)
     
 #Load yolo Model
-model = YOLO('yolov8n.pt')
+model = YOLO(r"C:\Users\Brandom\Documents\1Proyecto\yolo\scenario\runs\detect\train\weights\best.pt")
 
 #For each image, data will be stored as json
 resultData = []
-
+crudeResults  = []
 #Iterate file with the extension for images
 for nombre_archivo in nombres_archivos:
     ext = os.path.splitext(nombre_archivo)[-1].lower()
     if ext in extensiones_imagen:
-        path = directoryPath + "\\" +nombre_archivo
-        results = model(path)  #Evaluate YOLO model
+        path = os.path.join(directoryPath, nombre_archivo)
+        results = model(path, stream=False, verbose = False)  #Evaluate YOLO model
         for indvResults in results:
+            crudeResults.append(indvResults)
             resultData.append(json.loads(indvResults.tojson()))
 
 
-#Show data found
+
+#Show data found+
+fig, ax = plt.subplots()
 
 for i, imageData in enumerate(resultData):
-    print(f"Imagen {imageData[i]['path']}: Clases encontradas")
+    plt.close()
+    print(f"Imagen {nombres_archivos[i]}: Clases encontradas: ")
     last = ""
+    confidence_umbral = 0.91
     for object in imageData:
-        if last != object['name']:
-            last = object['name']
-            print("     >Nombre:", object['name'])
-        
-        #print("     >Clase:", object['class'])
-        #print("     >Confianza:", object['confidence'])
-        #print("     >Coordenadas de la caja delimitadora:", object['box'])
-    print("\n\n")
+        if object['confidence'] >= confidence_umbral:
+            if last != object['name']:
+                last = object['name']
+                print("     >Nombre:", object['name'])
+            
+            print("     >Clase:", object['class'])
+            print("     >Confianza:", object['confidence'])
+            print("     >Coordenadas de la caja delimitadora:", object['box'])
+
+            option = input("    >Show image classified? Y/N").upper()
+            if option == "Y":
+                im_array = crudeResults[i].plot()  # plot a BGR numpy array of predictions
+                im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+                plt.imshow(im)  # show image
+                plt.show
+                option = "fD3&gA9#pK2rS5yNqX3pX9!qZw2B__AT_INDEX:_7711521463pX9!qZw2B"
+            
+        else:
+            print(f"    >None")  
+            continue
+                
+
+      
+    plt.show()
+    print("\n")
 
